53a9157064d50fcad6efe0a4aa48d9b22d958a44
Disha Pareek <pareekdisha1105@gmail.com>
2021-06-09 23:33:40 +0530

    Importing libraries/reading data from dataset/reseting train

---
+++ project.py
@@ -0,0 +1,24 @@
+#Import Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from scipy.special import boxcox1p
+import seaborn as sns
+
+#reading data from dataset(csv files)
+features=pd.read_csv("features.csv")
+stores=pd.read_csv("stores.csv")
+train=pd.read_csv("train.csv")
+test=pd.read_csv("test.csv")
+#reseting train
+train=train.groupby(['Store','Date'])['Weekly_Sales'].sum()
+train=train.reset_index()
+train.head(10)
+#merging train and feature
+data=pd.merge(train,features,on=['Store','Date'],how='inner')
+data.head(10)
+#marging store with data
+data=pd.merge(data,stores,on=['Store'],how='inner')
+data.head(10)
